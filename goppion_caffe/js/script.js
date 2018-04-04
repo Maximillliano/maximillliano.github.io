588870e4a0c71b12613f6a5333fcf60dfac7285f
Maximillliano <maximilian_antipenko@mail.ru>
2018-04-04 08:31:00 +0300

    Прелоудер и мелкие баги

--- goppion_caffe/js/script.js
+++ goppion_caffe/js/script.js
@@ -99,3 +99,11 @@ $(function() {
     }
 
 }); /* End Fn */
+
+
+  $(window).on('load', function () {
+    $preloader = $('.loaderArea'),
+      $loader = $preloader.find('.loader');
+    $loader.fadeOut();
+    $preloader.delay(350).fadeOut('slow');
+  });
